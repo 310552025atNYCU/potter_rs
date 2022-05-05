ae7a2aa842461a69efa067d640807f4fb7292258
jayzhan.cs10 <jayzhan.cs10@nycu.edu.tw>
2022-05-05 16:35:53 +0800

    add test_books
    Signed-off-by: jayzhan.cs10 <jayzhan.cs10@nycu.edu.tw>

--- src/books.rs
+++ src/books.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 pub fn compute_books_cost(books: Vec<i32>) -> i32 {
     let n = books.len() as i32;
     return n * 8
--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,14 @@ mod tests {
     use crate::books::compute_books_cost;
 
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-    #[test]
     fn test_one_book() {
         let books = vec![1];
         assert_eq!(compute_books_cost(books), 8);
+    }
+
+    #[test]
+    fn test_books() {
+        let books = vec![1, 2, 3, 4];
+        assert_eq!(compute_books_cost(books), 8 * 4);
     }
 }
