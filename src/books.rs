29eaa91dffc47b56b65752c15bf5c856eab49bf4
jayzhan.cs10 <jayzhan.cs10@nycu.edu.tw>
2022-05-05 16:45:24 +0800

    add discount
    Signed-off-by: jayzhan.cs10 <jayzhan.cs10@nycu.edu.tw>

--- src/books.rs
+++ src/books.rs
@@ -1,5 +1,18 @@
+use std::collections::HashSet;
+
 #[allow(dead_code)]
-pub fn compute_books_cost(books: Vec<i32>) -> i32 {
-    let n = books.len() as i32;
-    return n * 8
+pub fn compute_books_cost(books: Vec<i32>) -> f32 {
+    let n = books.len() as f32;
+    return n * 8.0 * (1.0 - compute_discount(books))
+}
+
+fn compute_discount(books: Vec<i32>) -> f32{
+    let s: HashSet<i32> = books.iter().cloned().collect();
+    return match s.len() {
+        1 => 0.0,
+        2 => 0.05,
+        3 => 0.1,
+        4 => 0.2,
+        _ => 0.25
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,18 @@ mod tests {
     #[test]
     fn test_one_book() {
         let books = vec![1];
-        assert_eq!(compute_books_cost(books), 8);
+        assert_eq!(compute_books_cost(books), 8.0);
     }
 
     #[test]
     fn test_books() {
+        let books = vec![1, 2];
+        assert_eq!(compute_books_cost(books), 8.0 * 2.0 * 0.95);
+        let books = vec![1, 2, 3];
+        assert_eq!(compute_books_cost(books), 8.0 * 3.0 * 0.9);
         let books = vec![1, 2, 3, 4];
-        assert_eq!(compute_books_cost(books), 8 * 4);
+        assert_eq!(compute_books_cost(books), 8.0 * 4.0 * 0.8);
+        let books = vec![1, 2, 3, 4, 5];
+        assert_eq!(compute_books_cost(books), 8.0 * 5.0 * 0.75);
     }
 }
