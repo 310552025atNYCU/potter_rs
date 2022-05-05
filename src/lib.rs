8c4d4543df099c4de13ae6ba9f3cd68012ca2027
jayzhan.cs10 <jayzhan.cs10@nycu.edu.tw>
2022-05-05 17:16:08 +0800

    one failed
    Signed-off-by: jayzhan.cs10 <jayzhan.cs10@nycu.edu.tw>

--- src/lib.rs
+++ src/lib.rs
@@ -26,5 +26,9 @@ mod tests {
     fn test_books_with_copies() {
         let books = vec![1, 1, 2];
         assert_eq!(compute_books_cost(books), 8.0 * 2.0 * 0.95 + 8.0 * 1.0);
+        let books = vec![1, 1, 2, 3];
+        assert_eq!(compute_books_cost(books), 8.0 * 3.0 * 0.9 + 8.0 * 1.0);
+        let books = vec![0, 1, 1, 2, 2, 3, 3, 4];
+        assert_eq!(compute_books_cost(books), 8.0 * 4.0 * 0.8 + 8.0 * 4.0 * 0.8);
     }
 }
