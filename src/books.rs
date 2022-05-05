371dcfcab95409c15b0ea1e0482d732c9a19fa03
jayzhan.cs10 <jayzhan.cs10@nycu.edu.tw>
2022-05-05 17:10:49 +0800

    greedy approach
    Signed-off-by: jayzhan.cs10 <jayzhan.cs10@nycu.edu.tw>

--- src/books.rs
+++ src/books.rs
@@ -2,13 +2,36 @@ use std::collections::HashSet;
 
 #[allow(dead_code)]
 pub fn compute_books_cost(books: Vec<i32>) -> f32 {
-    let n = books.len() as f32;
-    return n * 8.0 * (1.0 - compute_discount(books))
+    
+    let mut cnt: Vec<u32> = vec![0, 0, 0, 0, 0];
+    for &book_id in books.iter() {
+        if book_id >= 0 && book_id < 5 {
+            cnt[book_id as usize] += 1;
+        }
+    }
+
+    let mut total_cost: f32 = 0.0;
+    while cnt.len() > 0 {
+        println!("cnt: {:#?}", cnt);
+        let n = cnt.len() as f32;
+        let min_count = *cnt.iter().min().unwrap();
+        let discount_factor = 1.0 - discount_map(cnt.len());
+        total_cost += n * 8.0 * discount_factor * min_count as f32;
+        cnt.iter_mut().for_each(|num| *num -= min_count);
+        cnt = cnt.into_iter().filter(|&num| num > 0).collect()
+    }
+    
+    return total_cost
 }
 
+#[allow(dead_code)]
 fn compute_discount(books: Vec<i32>) -> f32{
     let s: HashSet<i32> = books.iter().cloned().collect();
-    return match s.len() {
+    return discount_map(s.len())
+}
+
+fn discount_map(n: usize) -> f32 {
+    return match n {
         1 => 0.0,
         2 => 0.05,
         3 => 0.1,
@@ -16,3 +39,14 @@ fn compute_discount(books: Vec<i32>) -> f32{
         _ => 0.25
     }
 }
+
+// fn compute_discount(books: Vec<i32>) -> f32{
+//     let s: HashSet<i32> = books.iter().cloned().collect();
+//     return match s.len() {
+//         1 => 0.0,
+//         2 => 0.05,
+//         3 => 0.1,
+//         4 => 0.2,
+//         _ => 0.25
+//     }
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,13 @@ mod tests {
         assert_eq!(compute_books_cost(books), 8.0 * 3.0 * 0.9);
         let books = vec![1, 2, 3, 4];
         assert_eq!(compute_books_cost(books), 8.0 * 4.0 * 0.8);
-        let books = vec![1, 2, 3, 4, 5];
+        let books = vec![0, 1, 2, 3, 4];
         assert_eq!(compute_books_cost(books), 8.0 * 5.0 * 0.75);
+    }
+
+    #[test]
+    fn test_books_with_copies() {
+        let books = vec![1, 1, 2];
+        assert_eq!(compute_books_cost(books), 8.0 * 2.0 * 0.95 + 8.0 * 1.0);
     }
 }
